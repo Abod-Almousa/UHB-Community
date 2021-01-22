94ad86c519da4de4e5828dd68f7572c178092fee
Abod-almousa <abal12351@gmail.com>
2021-01-22 19:48:35 +0300

    test comment

--- app/src/main/java/sa/edu/uhb/uhbcommunity/MainActivity.java
+++ app/src/main/java/sa/edu/uhb/uhbcommunity/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Test comment
+        // Test comment.
     }
 }
