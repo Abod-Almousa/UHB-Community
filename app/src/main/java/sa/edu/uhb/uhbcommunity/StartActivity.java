3d556303df25daf2bb87fda0c6f01ddc2c30c759
Dova76 <abood.dj.sa@gmail.com>
2021-01-23 21:22:52 +0300

    The Register & Login Action Buttons

--- app/src/main/java/sa/edu/uhb/uhbcommunity/StartActivity.java
+++ app/src/main/java/sa/edu/uhb/uhbcommunity/StartActivity.java
@@ -2,13 +2,45 @@ package sa.edu.uhb.uhbcommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private ImageView iv_logo;
+    private Button btn_login,btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+        iv_logo = findViewById(R.id.iv_logo);
+
+        // When click on register button
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,RegisterActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // When click on login button
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
