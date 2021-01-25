b817a176d9ea6b08bc6b8aa1b7d6da09671e1b8d
Abod-almousa <abal12351@gmail.com>
2021-01-25 13:11:04 +0300

    Reset pass Activity XML

---
+++ app/src/main/java/sa/edu/uhb/uhbcommunity/ResetPassActivity.java
@@ -0,0 +1,14 @@
+package sa.edu.uhb.uhbcommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass);
+    }
+}
