dc5cc05cbafee1a6cce2e46551cb5827261adcf3
Abod-almousa <abal12351@gmail.com>
2021-02-01 19:12:11 +0300

    Main Activity XML

---
+++ app/src/main/java/sa/edu/uhb/uhbcommunity/AddPostActivity.java
@@ -0,0 +1,14 @@
+package sa.edu.uhb.uhbcommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
